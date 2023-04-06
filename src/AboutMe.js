8020b58f605001fc07e9805001434c12c7503fb0
kuteekutee <bernardsng@gmail.com>
2023-04-06 18:00:16 +0800

    change bg

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,23 +1,20 @@
-
 const AboutMe = () => {
   return (
     <div className="container">
       <h3>About Me</h3>
       <hr className="hr" />
-      <div>
-      <p>I have more that 10 years of experience developing software applications for
-        several companies and have performed well in various development roles
-        as Systems Analyst, Web developer and Team-Leader roles.
+      <p>
+        I have more that 10 years of experience developing software applications
+        for several companies and have performed well in various development
+        roles as Systems Analyst, Web developer and Team-Leader roles.
       </p>
-      <p>Learning new skills and technologies have always been an important. To
-        continue to improve and apply new knowledge to build useful software is extremely
-        satisfying.
+      <p>
+        Learning new skills and technologies have always been an important. To
+        continue to improve and apply new knowledge to build useful software is
+        extremely satisfying.
       </p>
-      </div>
     </div>
-    
-    
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
