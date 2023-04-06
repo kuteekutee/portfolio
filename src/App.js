1e2f1d37cc4669074baf02968d0f2990bf9ae2cc
kuteekutee <bernardsng@gmail.com>
2023-04-06 18:17:30 +0800

    fixed a typo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
-import AboutMe from './AboutMe';
-import ContactMe from './ContactMe';
+import "./App.css";
+import AboutMe from "./AboutMe";
+import ContactMe from "./ContactMe";
 
 function App() {
   const name = "Bernard Sng";
   const profession = "Software Engineer";
   const fileID = "1JmrawLQecFLL9CSvVPz_Moj5SmAX75Qf";
-  const imageUrl = `https://drive.google.com/uc?id=${fileID}` 
+  const imageUrl = `https://drive.google.com/uc?id=${fileID}`;
   return (
     <>
-      <div className='imageContainer'>
-        <img className='image' src={imageUrl} alt="picure of bernard" />
+      <div className="imageContainer">
+        <img className="image" src={imageUrl} alt="Bernard Sng" />
         <h1>{name}</h1>
         <h2>{profession}</h2>
         <AboutMe />
         <ContactMe />
-    </div>
+      </div>
     </>
-    
   );
 }
 
